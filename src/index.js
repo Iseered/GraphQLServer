d202a95fe41eacfe256599089e695351186091b9
Iseered <mbrr2712@gmail.com>
2024-06-27 00:19:00 -0500

    Descripción de los cambios realizados

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-
+const { graphqlHTTP } = require('express-graphql');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 async function startServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
 
   const app = express();
+
+  // Configuración adicional para la ruta /api/graphql
+  app.use('/api/graphql', graphqlHTTP({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    graphiql: true,
+  }));
   server.applyMiddleware({ app });
 
   app.listen({ port: 4000 }, () =>
